8e0ae728a31468f49451ff15296eaa5c2a393f52
Mikyx <mavt1423@gmail.com>
2025-12-06 19:36:01 -0400

    agregando estilos al login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,27 @@
 const linkRegistro = document.getElementById('link-registro');
 const formLogin = document.getElementById('form-login');
 const formRegistro = document.getElementById('form-registro');
+const linkRecuperar = document.getElementById('link-recuperar');
+const formRecuperar = document.getElementById('form-recuperar');
+const linkLoginRegistro = document.getElementById('link-login-registro');
+const linkLoginRecuperar = document.getElementById('link-login-recuperar');
 
 linkRegistro.addEventListener('click', () => {
     formLogin.classList.add('ocultar');
     formRegistro.classList.remove('ocultar');
+});
+
+linkRecuperar.addEventListener('click', () =>{
+    formLogin.classList.add('ocultar');
+    formRecuperar.classList.remove('ocultar');
+});
+
+linkLoginRecuperar.addEventListener('click', () => {
+    formRecuperar.classList.add('ocultar');
+    formLogin.classList.remove('ocultar');
+});
+
+linkLoginRegistro.addEventListener('click', () => {
+    formRegistro.classList.add('ocultar');
+    formLogin.classList.remove('ocultar');
 });
